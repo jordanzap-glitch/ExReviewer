d832f12801caf199c1f872c9d413e60d7c27618f
jordanzap-glitch <nutkoko675@gmail.com>
2026-02-24 11:27:09 +0800

    refactor: Update footer and sidebar with new branding and remove unused links

--- pages/includes/footer.php
+++ pages/includes/footer.php
@@ -5,10 +5,7 @@
                     document.write(new Date().getFullYear());
                 </script>
             </p>
-            <p><span>By: <a target="_blank" href="https://wrapbootstrap.com/user/theme_ocean" target="_blank">theme_ocean</a></span> • <span>Distributed by: <a target="_blank" href="https://themewagon.com" target="_blank">ThemeWagon</a></span></p>
+            <p><span>By: <a target="_blank" href="" target="_blank">PSU Students</a></span> • <span>Distributed by: <a target="_blank" href="" target="_blank">Group #</a></span></p>
             <div class="d-flex align-items-center gap-4">
-                <a href="javascript:void(0);" class="fs-11 fw-semibold text-uppercase">Help</a>
-                <a href="javascript:void(0);" class="fs-11 fw-semibold text-uppercase">Terms</a>
-                <a href="javascript:void(0);" class="fs-11 fw-semibold text-uppercase">Privacy</a>
             </div>
 </footer>
